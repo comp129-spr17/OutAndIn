f458eeb18e860793745d7ac1e5334b21f71c2d1f
MalvikaSriram <m_sriram@u.pacific.edu>
2017-02-10 22:05:00 -0800

    commiting chat change

---
+++ sandbox/src/components/Chat/index.js
@@ -0,0 +1,14 @@
+import React, { PropTypes, Component } from 'react'; 
+
+export default class Chat extends Component {
+	 	render() {
+       	return (
+            <div> 
+                <h1>
+					Hello, here we will create a chat box.
+				</h1>
+          	</div>
+     	);
+	}
+
+}
