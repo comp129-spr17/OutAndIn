035476c33b2dc7f7ab09f229ae0ac12b8ce47466
MalvikaSriram <m_sriram@u.pacific.edu>
2017-02-11 20:48:20 -0800

    Displaying message input box and send button on chat route index file.

--- sandbox/src/components/Chat/index.js
+++ sandbox/src/components/Chat/index.js
@@ -1,12 +1,37 @@
 import React, { PropTypes, Component } from 'react'; 
 
-export default class Chat extends Component {
-	 	render() {
+export default class Chat extends Component { 
+		constructor () { //constructor
+	 			super(); //parent class(Component)
+	 			this.state= { //new object
+	 				value: '',
+	 				holder:[]
+	 			}; 
+	 			this.handleTextSend = this.handleTextSend.bind(this);
+	 			this.handleChange = this.handleChange.bind(this);
+	 		}
+	 		    handleChange(event) {
+	    this.setState({value: event.target.value})
+	}
+
+	handleTextSend(event) {
+		event.preventDefault();
+		if(this.state.value=='') //resetting value of this.state.value 
+			return;
+		var obj = {from:'Abc', text:this.state.value};
+		this.state.holder.push(obj);
+		if(this.state.holder.length > 10 ) this.state.holder.shift();
+		 this.setState({value: ''})
+		this.forceUpdate();
+	}
+
+	 	render() { 		
        	return (
             <div> 
-                <h1>
-					Hello, here we will create a chat box.
-				</h1>
+         <form className='form'>
+            <input type="text" value={this.state.value} onChange={this.handleChange} autoComplete="off" className='msg' placeholder='message'/>
+            <button onClick={this.handleTextSend}>Send</button>
+        </form>
           	</div>
      	);
 	}
