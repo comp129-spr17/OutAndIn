e7f3d6af60280fbea8f04b86ae9e91715ef62c3e
MalvikaSriram <m_sriram@u.pacific.edu>
2017-02-13 17:10:44 -0800

    small issue with cmd.

--- sandbox/src/components/Chat/index.js
+++ sandbox/src/components/Chat/index.js
@@ -12,31 +12,32 @@ export default class Chat extends Component {
 	 			this.handleChange = this.handleChange.bind(this);
 	 		}
 	 		    handleChange(event) {
-	    this.setState({value: event.target.value})
+	    this.setState({value: event.target.value})  //setting value of this.state.value to what is typed in input box
 	}
 
-	handleTextSend(event) {
+	handleTextSend(event) {  //storing chat in array
 		event.preventDefault();
 		if(this.state.value=='') //checking if value is empty
 			return;
 		var obj = {from: this.state.name, text:this.state.value};
-		this.state.holder.push(obj);
-		if(this.state.holder.length > 10 ) this.state.holder.shift(); //holds last ten chats 
+		this.state.holder.push(obj);  //pushing name of sender and chat in array (add to array)
+		if(this.state.holder.length > 10 ) this.state.holder.shift(); //holds last ten chats. If size is greater than 10, popping oldest message every time a new one is sent(remove from array).
 		 this.setState({value: ''})
 		this.forceUpdate();
 	}
 
 	 	render() { 		//form for input message and send button creation)
 	 		if(this.state.name == ''){
-				var person = prompt("Please enter your name", this.state.name);
-				this.state.name=person;
-			if(!person) this.state.name="Anon";
+				var person = prompt("Please enter your name", this.state.name); //var person stores user input, which is name
+				this.state.name=person; //setting name input from person var to the name var
+			if(!person) this.state.name="Anon"; //if name is not entered, user is an anon 
 		}
        	return (
+       		//unordered list tag
             <div> 
             <div>
-    			<ul className="messages" >
-    				{this.state.holder.map((msg, k) => {
+    			<ul className="messages" > 
+    				{this.state.holder.map((msg, k) => { //mapping each element of holder to item in list(li)
     				return <li key={k}><span className='msgSender'>{msg.from}:</span> {msg.text}</li>
     				})}
    			   </ul>
