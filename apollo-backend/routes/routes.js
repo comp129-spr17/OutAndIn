ae1e8d7ce96d0f1f1282df8b0ec50d1180c687ac
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-02-18 19:33:16 -0800

    Removed Dockerfile

--- apollo-backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var messaging = require('../controllers/messaging');
-
-router.use('/messagingHandler', messaging);
-
-
-module.exports = router;
