a1d1df43d2f56c28534b6a56b5934cd6759432de
pranavt61 <p_thirunavukkarasu@u.pacific.edu>
2017-02-20 23:27:01 -0800

    User creation backend set up

--- apollo-backend/server/controllers/messaging.js
+++ apollo-backend/server/controllers/messaging.js
@@ -73,34 +73,16 @@ var Schemas = {
 	}
 };
 
-var msgSocket = function(socket){
-	//define websockets here
+//on initial connect
+//namespace: UserInit
+/*input:
+{
+	name: 'name'
+}
+*/
+module.exports.connect = function(data){
+	//create user 
+	new Schemas.User(data.name);
+	console.log('User added: ' + Lists.User.toString());
 };
 
-
-var express = require('express');
-var router = express.Router();
-
-var globalMess = globalMess || [];
-
-router.post('/global', function(req, res) {
-    //Error handling
-    if(!req.body.user)
-        throw {message: "No user given"};
-    if(!req.body.message)
-        throw {message: "no message given"};
-    if(typeof req.body.user != 'string')
-        throw {message: "User is not a string"}
-    if(typeof req.body.message != 'string')
-        throw {message: "Message is not a string"}
-
-	globalMess.push({
-		user: req.body.user,
-		message: req.body.message,
-		timeStamp: Math.floor(Date.now() / 1000) // UNIX timestamp
-	});
-
-	res.send(JSON.stringify(globalMess));
-});
-
-module.exports = router;
--- apollo-backend/server/controllers/sockets.js
+++ apollo-backend/server/controllers/sockets.js
@@ -1,5 +1,16 @@
+var paths = require('./messaging');
+
 var sockets = function(io){
-    console.log("yo in sockets");
+	
+	//namespaces
+	var userInitNS = io.of('/UserInit');
+	userInitNS.on('connection', function(socket){
+		console.log('User connected to UserINit');
+		socket.on('addUser', function(data){
+			paths.connect(data);
+		});
+	});
+	
     io.on('connection', function(socket){
         console.log("User connected");
         socket.on('disconnect', function(){
