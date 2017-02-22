f626af4b625ec9ebe0828a930cd975200f023ad4
pranavt61 <p_thirunavukkarasu@u.pacific.edu>
2017-02-21 19:11:52 -0800

    added socket event functions

--- apollo-backend/server/controllers/messaging.js
+++ apollo-backend/server/controllers/messaging.js
@@ -18,7 +18,47 @@ var LastID = {
 //TODO: move all data storage to mySQL
 var Lists = {
 	User:[],
-	Chat:[]
+	Chat:[],
+	getUserIds:function(){
+		var arr = [];
+		for(var i = 0; i < Lists.User.length; i ++)
+			arr.push(Lists.User[i].id);
+		
+		return arr;
+	},
+
+	getUser: function(id){
+		//error checking
+		if(typeof id != 'number')
+			return {error: 'invalid id'};	
+		
+		for(var i = 0; i < Lists.User.length; i ++)
+			if(Lists.User[i].id == id)
+				return {user : Lists.User[i]};
+
+		return {error: 'No user found'};
+	},
+	
+	getChatIds: function(){
+		var arr = [];
+		for(var i = 0; i < Lists.Chat.length; i ++)
+			arr.push(Lists.Chat[i].id);
+
+		return arr;
+	},	
+
+	getChat: function(id){
+		//error checking
+		if(typeof id != 'number')
+			return {error: 'invalid id'};	
+		
+		for(var i = 0; i < Lists.Chat.length; i ++)
+			if(Lists.Chat[i].id == id)
+				return {user : Lists.Chat[i]};
+
+		return {error: 'No chat found'};
+
+	}
 };
 
 //constructs data
@@ -41,7 +81,7 @@ var Schemas = {
 			user1 = -1;
 		if(typeof user2 != 'number')
 			user2 = -1;	
-
+ 
 		//ref to self
 		var _self = this;
 
@@ -73,16 +113,118 @@ var Schemas = {
 	}
 };
 
+//export function objects
+var exp = module.exports;
+
+exp.User = {};
+exp.Chat = {};
+
 //on initial connect
 //namespace: UserInit
-/*input:
+/*
+input:
 {
 	name: 'name'
 }
+
+output:
+{
+	userIds: []
+}
 */
-module.exports.connect = function(data){
+exp.User.add = function(data, socket){
+	//error checking
+	if(typeof data.name != 'string')
+	{
+		socket.emit('error', {
+			error: 'User.add: Invalid user name'
+		});
+		return;
+	}
+
 	//create user 
 	new Schemas.User(data.name);
-	console.log('User added: ' + Lists.User.toString());
+	
+	//send new user list to all clients
+	socket.emit('userIdList', {
+		userIds: Lists.getUserIds()
+	});
 };
 
+/*
+input:
+{
+	id: 'user id'
+}
+
+outputs: 
+{
+	id: 'user id',
+	name: 'user name'
+}
+*/
+exp.User.get = function(data, socket){
+	//error checking
+	if(typeof data.id != 'number')
+	{
+		socket.emit('error'{
+			error: 'User.get: invalid user id',
+			id: data.id
+		});
+		return;
+	}
+
+	var user = Lists.getUser(data.id).name;
+
+	if(user.error != null){
+		//send user from id
+		socket.emit('userData', {
+			id: data.id, 
+			name: Lists.getUser(data.id).name
+		});
+	}else{
+		socket.emit('error', {
+			errors: 'User.get: no user found',
+			id: data.id
+		});
+	}
+};
+
+/*
+input:
+{
+	name: 'chat name',
+	user1: 'user id',
+	user2: 'user id'
+}
+output: 
+{
+	chatIDs: []
+}
+*/
+exp.Chat.add = function(data, socket){
+	//error checking
+	var errors = '';
+	if(typeof data.name != 'string')
+		errors += 'invalid chat name; ';
+	if(typeof data.user1 != 'number')
+		errors += 'invlid user1 id; ';
+	if(typeof data.user2 != 'number')
+		errors != 'invalid user2 id';
+	if(errors != '')
+	{
+		socket.emit('error', {
+			error: 'Chat.add: ' + errors,
+			name: data.name,
+			user1: data.user1,
+			user2: data.user2
+		});
+	}
+	
+	socket.emit('chatIdList'{
+		chatIds: Lists.getChatIds()
+	});
+
+};
+
+//TODO: getChat, addMessage
