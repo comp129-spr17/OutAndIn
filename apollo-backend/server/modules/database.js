731f0c3e8c196821b4d1899acf5a9f3ed71ebe94
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-02-21 16:56:35 -0800

    Created Docker database initialization

--- apollo-backend/server/modules/database.js
+++ apollo-backend/server/modules/database.js
@@ -1,4 +1,39 @@
 var mysql = require('mysql');
+var exec = require('child_process').execFile;
+
+var init = function(){
+    exec("./database/start_db.sh", null, null, function(error, stdout, stderr) {
+        if(error)
+            console.error("Error: " + error);
+        if(stdout)
+            //console.error("Stdout: " + stdout);
+        if(stderr)
+            console.error("stderr: " + stderr);
+        console.log("Starting MySQL Docker Container");
+    });
+    if (process.platform === "win32") {
+        var rl = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.on("SIGINT", function () {
+            process.emit("SIGINT");
+        });
+    }
+    process.on("SIGINT", function () {
+        //graceful shutdown
+        exec("./database/stop_db.sh", null, null, function(error, stdout, stderr) {
+            if(error)
+                console.error("Error: " + error);
+            if(stdout)
+                console.error("Stdout: " + stdout);
+            if(stderr)
+                console.error("stderr: " + stderr);
+            console.error("Stopping MySQL Docker Container");
+        });
+        process.exit();
+    });
+};
 
 var pool = mysql.createPool({
     host: 'localhost',
@@ -9,4 +44,5 @@ var pool = mysql.createPool({
     supportBigNumbers: true,
 });
 
+exports.init = init;
 exports.pool = pool;
