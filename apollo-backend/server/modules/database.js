581a076b5ba58be353352bcd3a3765e8ef621817
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-02-23 01:23:59 -0800

    Created database initialization functions

--- apollo-backend/server/modules/database.js
+++ apollo-backend/server/modules/database.js
@@ -1,61 +1,115 @@
-var mysql = require('mysql');
-var exec = require('child_process').exec;
+var mysql = require('promise-mysql');
+var fs = require('fs');
+var Promise = require('bluebird');
+
+// Temporary MySQL connection to initalize the database
+var tmpConnection;
+// Permenant MySQL connection pool
+var pool;
+// Database name
+var databaseName = "apollo"; 
+
+function sleep(time) {
+    return new Promise((resolve, reject) => setTimeout(resolve, time));
+}
+
+function checkIfDatabaseExists(){
+    return tmpConnection.getConnection().then((connection) => {
+        var sql = "SHOW DATABASES";
+        return connection.query(sql);
+    }).then((results) => {
+        // Check if any databases exists
+        if(results.length == 0) {
+            return false;
+        }
+        // Check if apollo database exists
+        for(result in results){
+            if(results[result]['Database'] == databaseName){
+                return true; 
+            }
+        }
+        return false;
+    });
+}
+
+function dropDatabase(){
+    return tmpConnection.getConnection().then((connection) => {
+        var sql = "DROP DATABASE " + databaseName;
+        return connection.query(sql);
+    }).then((results) => {
+        console.log("DATABASE: Database '" + databaseName + "' dropped successfully");
+        return;
+    }).catch((err) => {
+        console.log(err); 
+    });
+}
+
+function createDatabase(){
+    tmpConnection.getConnection().then((connection) => {
+        var sql = "CREATE DATABASE " + databaseName + " CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+        return connection.query(sql);
+    }).then((results) => {
+        console.log("DATABASE: Created the '" + databaseName + "' database");
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+function createTables(sqlDumpFilePath){
+    setTimeout(() => {
+        var importer = require('node-mysql-importer');
+        var myCon = importer.config({
+            host: 'localhost',
+            user: 'root',
+            password: 'apollo',
+            database: 'apollo'
+        });
+        importer.importSQL('./database/apollo-database.sql').then(() => {
+            console.log("all statement executed"); 
+        }).catch((err) => {
+            console.log(err) 
+        });
+    }, 2000);
+}
 
 var init = function(){
-    exec("docker run --name apollo-mysql -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=apollo centurylink/mysql", function(error, stdout, stderr) {
-        if(error)
-            console.error("Error: " + error);
-        if(stdout)
-            //console.error("Stdout: " + stdout);
-        if(stderr)
-            console.error("stderr: " + stderr);
-        console.log("Starting MySQL Docker Container");
+    // Create temporary mysql connection
+    tmpConnection = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: 'apollo', // running locally, no security concerns here
+        connectionLimit: 200,
+        supportBigNumbers: true
     });
-    if (process.platform === "win32") {
-        var rl = require("readline").createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        rl.on("SIGINT", function () {
-            process.emit("SIGINT");
-        });
-    }
-    process.on("SIGINT", function () {
-        if(process.platform === "win32") {
-            exec('set DID=docker ps -aqf "name=apollo-mysql" && docker stop $Env:DID && docker rm $Env:DID', function(error, stdout, stderr) {
-                if(error)
-                    console.error("Error: " + error);
-                if(stdout)
-                    console.error("Stdout: " + stdout);
-                if(stderr)
-                    console.error("stderr: " + stderr);
-                console.error("Stopping MySQL Docker Container");
-            }); 
-            process.exit();
-            return;
+    
+    // Check if 'apollo' database exists
+    checkIfDatabaseExists().then((exists) => {
+        if(exists) {
+            console.log("DATABASE: Database '" + databaseName + "' exists");
+            return dropDatabase();
         }
-        //graceful shutdown
-        exec('DID=`docker ps -aqf "name=apollo-mysql"` && docker stop $DID && docker rm $DID', function(error, stdout, stderr) {
-            if(error)
-                console.error("Error: " + error);
-            if(stdout)
-                console.error("Stdout: " + stdout);
-            if(stderr)
-                console.error("stderr: " + stderr);
-            console.error("Stopping MySQL Docker Container");
+        console.log("DATABASE: Database '" + databaseName + "' does not exists");
+    }).then(() => {
+        return createDatabase(); 
+    }).then(() => {
+        return; 
+    }).catch((err) => {
+        console.log(err); 
+    }).finally(() => {
+        // Create 'apollo' database tables
+        var sqlDumpFilePath = './database/apollo-database.sql';
+        createTables(sqlDumpFilePath);
+        // Create pool of mysql connections
+        pool = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: 'apollo', // running locally, no security concerns here
+            database: 'apollo',
+            connectionLimit: 200,
+            supportBigNumbers: true
         });
-        process.exit();
     });
 };
-
-var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'username',
-    database: 'dbName',
-    connectionLimit: 200,
-    supportBigNumbers: true,
-});
 
 exports.init = init;
 exports.pool = pool;
