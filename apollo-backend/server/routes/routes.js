b5b8b5095be6567afc80fef6754ad18bc9771060
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-02-23 14:46:07 -0800

    Added user route for testing

--- apollo-backend/server/routes/routes.js
+++ apollo-backend/server/routes/routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var messaging = require('../controllers/messaging');
+var users = require('../controllers/users');
 
 //router.use('/messagingHandler', messaging);
-
+router.use('/users', users);
 
 module.exports = router;
