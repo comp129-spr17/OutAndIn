663e3ff12f93df29507646fae0de19541941b64b
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-02-25 02:06:34 -0800

    Getting the pool creation params directly from the config file

--- apollo-backend/server/modules/database.js
+++ apollo-backend/server/modules/database.js
@@ -1,9 +1,11 @@
 var mysql = require('promise-mysql');
+var config = require('../config/config');
 
 var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'apollo', // running locally, no security concerns here
+    host: config['APOLLO_MYSQL_IP'],
+    user: config['APOLLO_MYSQL_USER'],
+    password: config['APOLLO_MYSQL_PASS'],
+    port: config['APOLLO_MYSQL_PORT'],
     database: 'apollo',
     connectionLimit: 200,
     supportBigNumbers: true
