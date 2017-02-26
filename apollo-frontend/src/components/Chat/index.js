9bab6a6e55cd8bec138be93f871cc2acaf776e50
MalvikaSriram <m_sriram@u.pacific.edu>
2017-02-25 23:10:34 -0800

    User's name is now 'me' on chats. hoping this will work when chats are displayed on the screen again

--- apollo-frontend/src/components/Chat/index.js
+++ apollo-frontend/src/components/Chat/index.js
@@ -100,7 +100,7 @@ class RightChatComponent extends React.Component{
 	render(){
 		return(
 			<div className="bubble-right">
-				<span className='msgSender'>{this.props.msg.user}: </span> {this.props.msg.message} <br/> <span className='msgTimeStamp'>{moment.unix(this.props.msg.timeStamp).fromNow()} </span>
+				<span className='msgSender'> me : </span> {this.props.msg.message} <br/> <span className='msgTimeStamp'>{moment.unix(this.props.msg.timeStamp).fromNow()} </span>
 			</div>
 		)
 	}
