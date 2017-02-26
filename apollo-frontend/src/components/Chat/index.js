ab4cead57cea7993e9513b09c6dc607a0be78f67
MalvikaSriram <m_sriram@u.pacific.edu>
2017-02-25 23:45:48 -0800

    Formatting sidebar component arrangements

--- apollo-frontend/src/components/Chat/index.js
+++ apollo-frontend/src/components/Chat/index.js
@@ -79,7 +79,7 @@ class SideBar extends React.Component {
   render() {
     return <div className="div-left" >
         		{this.props.friends.map((friend, k) => { 
-                			return <div key={k}> <img className="img-circle" src="https://www.abeautifulsite.net/content/uploads/2014/08/rounded-image-250x250.png"/> <p> {friend} </p> </div>
+                			return <div className="sidebar-container" key={k}> <img className="img-circle" src="https://www.abeautifulsite.net/content/uploads/2014/08/rounded-image-250x250.png"/> <p className="sidebar-name"> {friend} </p> </div>
 	    			})
         		}
         	</div>;
