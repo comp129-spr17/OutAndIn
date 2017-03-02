a34f5a80e657059438e56fff94b45ae1502a0049
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-01 21:30:10 -0800

    Restructured how the router is called

--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
-import App from './components/app';
-import Friends from './components/Friends';
-import NotFound from './components/NotFound';
+import App from './components/App';
+import Home from './pages/Home';
 import Chat from './components/Chat';
+import NotFound from './components/NotFound';
 
-const Routes = (props) => (
-  	<Router {...props}>
-		<Route path="/" component={App} />
-		<Route path="/friends" component={Friends} />
-			<Route path="/chat" component={Chat} /> 
-		<Route path="*" component={NotFound} />
-   	</Router>
+export default (
+    <Route path="/" component={App} >
+        <IndexRoute component={Home} />
+		<Route path="/chat" component={Chat} /> 
+        <Route path="*" component={NotFound} />
+    </Route>
 );
-
-export default Routes;
