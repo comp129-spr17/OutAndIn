97a8d418b2d52a413663cc0622a8cdcaf1285202
pranavt61 <p_thirunavukkarasu@u.pacific.edu>
2017-03-01 23:32:45 -0800

    moved stuff fo real

---
+++ apollo-backend/controllers/sockets.js
@@ -0,0 +1,52 @@
+var paths = require('./messaging');
+
+var sockets = function(io) {
+	
+	//namespaces
+	var chatSocket = io.of('/Chat');
+	chatSpace(chatSocket);
+    var globalMessage = globalMessage || [];
+
+    io.on('connection', function(socket){
+        console.log("User connected");
+        socket.on('message', function(data){
+            globalMessage.push({
+                user: data.user,
+                message: data.message,
+                timeStamp: Math.floor(Date.now() / 1000) // UNIX timestamp
+            });
+            io.emit('message', JSON.stringify(globalMessage));
+        });
+
+		socket.on('addUser', function(data){
+			console.log("EVENT: addUser");
+           	paths.User.add(data, chatSock); 
+		});
+
+		socket.on('getUser', function(data){
+			console.log("EVENT: getUser");
+           	//paths.User.get(data, chatSock, socket.id);
+		});	
+
+		socket.on('addChat', function(data){
+			console.log("EVENT: addChat");
+           	//paths.Chat.add(data, chatSock);
+		});
+
+		socket.on('getChat', function(data){
+			console.log("EVENT: getChat");
+           	//paths.Chat.get(data, chatSock, socket.id);
+		});
+
+		socket.on('addMessage', function(data){
+			console.log("EVENT: addMessage");
+           	//paths.Chat.addMessage(data, chatSock);
+		});
+
+        socket.on('disconnect', function(){
+            console.log("User disconnected");
+        });
+    });   
+};
+
+module.exports = sockets;
