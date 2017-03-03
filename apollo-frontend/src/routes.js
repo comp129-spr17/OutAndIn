40f0a70d33f18558af7e6fc552271030cfd90ef6
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-02 23:19:43 -0800

    Changed how routes are exported

--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import Chat from './components/Chat';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 
-const Routes = (props) => (
-  	<Router {...props}>
-		<Route path="/" component={App} />
+export default (
+    <Route path="/" component={App} >
+        <IndexRoute component={Home} />
 		<Route path="/home" component={Home} />
 		<Route path="/chat" component={Chat} /> 
-		<Route path="*" component={NotFound} />
-   	</Router>
+        <Route path="*" component={NotFound} />
+    </Route>
 );
-
-export default Routes;
