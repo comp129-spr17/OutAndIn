bf8879031c4da91a3f4d86fb0a3d7330c1bf5898
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-02 22:49:53 -0800

    Added new services for user actions

--- apollo-backend/services/users.js
+++ apollo-backend/services/users.js
@@ -2,7 +2,17 @@
 var db = require('../modules/database');
 var Promise = require('bluebird');
 
-exports.getAllUsers = function(){
-    var sql = "SELECT * FROM user";
+exports.usersGetAll = function(){
+    var sql = "SELECT * FROM users";
     return db.pool.query(sql);
+};
+
+exports.usersCreateUser = function(username){
+    var sql = "INSERT INTO users VALUES ('', '', '', '', ?, '', '')";
+    return db.pool.query(sql, [username]);
+};
+
+exports.usersGetUserByUsername = function(username){
+    var sql = "SELECT id FROM users WHERE username = ?";
+    return db.pool.query(sql, [username]);
 };
