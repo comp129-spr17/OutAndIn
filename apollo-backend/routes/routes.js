cbd7eb3948a4b0b5ce002ad3c865d80f461dea85
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-02 22:49:27 -0800

    Included test route for chats

--- apollo-backend/routes/routes.js
+++ apollo-backend/routes/routes.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var messaging = require('../controllers/messaging');
 var users = require('../controllers/users');
+var chats = require('../controllers/chats');
 
-//router.use('/messagingHandler', messaging);
 router.use('/users', users);
+router.use('/chats', chats);
 
 module.exports = router;
