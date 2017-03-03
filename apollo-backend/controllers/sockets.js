f5370118df3f842494dab4384bd3dd355c8f94e1
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-02 22:50:21 -0800

    Added handlers for username registration

--- apollo-backend/controllers/sockets.js
+++ apollo-backend/controllers/sockets.js
@@ -1,4 +1,5 @@
 var paths = require('./messaging');
+var usersService = require('../services/users');
 
 var sockets = function(io) {
 	
@@ -13,10 +14,53 @@ var sockets = function(io) {
                 message: data.message,
                 timeStamp: Math.floor(Date.now() / 1000) // UNIX timestamp
             });
+            console.log("USER: " + data.user);
             io.emit('message', JSON.stringify(globalMessage));
         });
 
-		socket.on('addUser', function(data){
+        socket.on('userInit', function(data){
+            usersService.usersGetUserByUsername(data["details"]["username"]).then((res) => {
+                if(res.length == 0) {
+                    return usersService.usersCreateUser(data["details"]["username"]);             
+                }
+                // Username already exists
+                throw 0;
+            }).then((res) => {
+                let response = {
+                    "object": "USER",
+                    "action": "INIT",
+                    "code": "1",
+                    "message": "Username acquired successfully",
+                    "details": {
+                        username: data["details"]["username"]
+                    }
+                };
+                socket.emit('userInit', JSON.stringify(response));
+            }).catch((err) => {
+                if(err == 0){
+                    let response = {
+                        "object": "USER",
+                        "action": "INIT",
+                        "code": "0",
+                        "message": "Username is already taken",
+                        "details": {}
+                    };
+                    socket.emit('userInitError', JSON.stringify(response));    
+                    return;
+                }
+                let response = {
+                    "object": "USER",
+                    "action": "INIT",
+                    "code": "2",
+                    "message": "Database Error",
+                    "details": {}
+                };
+                socket.emit('userInitError', JSON.stringify(response));           
+            });   
+        });
+
+        socket.on('addUser', function(data){
+            
 			console.log("EVENT: addUser");
            	paths.User.add(data, socket); 
 		});
@@ -26,7 +70,10 @@ var sockets = function(io) {
            	//paths.User.get(data, chatSock, socket.id);
 		});	
 
-		socket.on('addChat', function(data){
+        socket.on('joinChat', function(data){
+            // check if chat is already established between those two users
+            // return that chat if there is
+            // if not, make new chat
 			console.log("EVENT: addChat");
            	//paths.Chat.add(data, chatSock);
 		});
