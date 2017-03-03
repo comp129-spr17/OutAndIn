073a6a0930b1a7a3cc12cb96c9eeb5cf1cdd7ed1
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-02 23:18:50 -0800

    Setting up component to be a layout

--- apollo-frontend/src/components/App.js
+++ apollo-frontend/src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <div> App </div>
+                <div>{this.props.children}</div>
             </div>
         )
     }
