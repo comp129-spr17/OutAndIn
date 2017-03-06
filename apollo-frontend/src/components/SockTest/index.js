d1979cb699e18ac0c3afb6117aabad636217c378
pranavt61 <p_thirunavukkarasu@u.pacific.edu>
2017-03-05 20:20:01 -0800

    readding testing ground

---
+++ apollo-frontend/src/components/SockTest/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class SockTest extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+    }
+
+    render() {
+        return (<div><h3>WORKS</h3></div>);
+    }
+}
--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -4,13 +4,15 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Chat from './components/Chat';
 import Home from './components/Home';
+import SockTest from './components/SockTest'
 import NotFound from './components/NotFound';
 
 export default (
     <Route path="/" component={App} >
-        <IndexRoute component={Home} />
+    <IndexRoute component={Home} />
 		<Route path="/home" component={Home} />
-		<Route path="/chat" component={Chat} /> 
-        <Route path="*" component={NotFound} />
+		<Route path="/chat" component={Chat} />
+		<Route path="/sockTest" component={SockTest} />
+    <Route path="*" component={NotFound} />
     </Route>
 );
