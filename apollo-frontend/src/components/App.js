11c2663356b8a871e5ec746f3c4f5710f195f845
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-07 01:16:46 -0800

    Rearranged the layout

--- apollo-frontend/src/components/App.js
+++ apollo-frontend/src/components/App.js
@@ -8,15 +8,11 @@ class App extends Component {
     render() {
         return (
             <div className="apollo-container">
-                <div className="container-fluid">
-                    <div className="row">
-                        <Header />
-                        <Sidebar />
-                        <div className="main">
-                            <div className="col-xs-12">
-                                { this.props.children }
-                            </div>
-                        </div>
+                <Header />
+                <Sidebar />
+                <div className="main">
+                    <div className="col-xs-12">
+                        { this.props.children }
                     </div>
                 </div>
             </div>
