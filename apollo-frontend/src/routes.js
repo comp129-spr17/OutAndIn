6f7890e409f1407077b80f6fbc578bf933f86bfb
MalvikaSriram <m_sriram@u.pacific.edu>
2017-03-12 11:46:40 -0700

    Created login form. Still need to fix display and move sidebar component out of app.js

--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -3,8 +3,9 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import Chat from './components/Chat';
+import Login from './components/Login';
 import Home from './components/Home';
-import SockTest from './components/SockTest'
+import SockTest from './components/SockTest';
 import NotFound from './components/NotFound';
 
 export default (
@@ -13,6 +14,8 @@ export default (
 			<Route path="/home" component={Home} />
 			<Route path="/chat" component={Chat} />
 			<Route path="/sockTest" component={SockTest} />
+			<Route path="/login" component={Login} />
+
     	<Route path="*" component={NotFound} />
     </Route>
 );
