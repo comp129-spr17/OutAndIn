78b5832ee87a9ae8420a52e0e880dd7629133a91
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-12 12:47:06 -0700

    Added two distinct layouts

---
+++ apollo-frontend/src/components/Layouts/Blank.js
@@ -0,0 +1,19 @@
+import '../../styles/app.scss';
+import React, { Component } from 'react';
+import { client } from '../../modules/api-client';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+
+class Blank extends Component {
+    render() {
+        return (
+            <div className="apollo-container">
+                <div className="col-xs-12">
+                    { this.props.children }
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Blank;
---
+++ apollo-frontend/src/components/Layouts/Main.js
@@ -0,0 +1,23 @@
+import '../../styles/app.scss';
+import React, { Component } from 'react';
+import { client } from '../../modules/api-client';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="apollo-container">
+                <Header />
+                <Sidebar />
+                <div className="main">
+                    <div className="col-xs-12">
+                        { this.props.children }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Main;
