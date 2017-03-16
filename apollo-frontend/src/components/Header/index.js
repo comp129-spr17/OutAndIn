a5465ac71ae4fbd256fbe9fbbefc3abe0d4597f3
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-16 11:35:37 -0700

    Added header navigation links

--- apollo-frontend/src/components/Header/index.js
+++ apollo-frontend/src/components/Header/index.js
@@ -7,6 +7,19 @@ export default class Header extends Component {
                 <div className="logo-container">
                     <img className="logo" src="/img/logo.png" alt="Apollo Logo"/>
                 </div>
+                <div className="info-container">
+                    <div className="info-links">
+                        <span><a href=""><i className="fa fa-user"></i></a></span>
+                        <span><a href=""><i className="fa fa-cog"></i></a></span>
+                        <span><a href=""><i className="fa fa-sign-out"></i></a></span>
+                    </div>
+                    <div className="info-user">
+                        <div className="info-img">
+                            <img src="/img/avatar.jpg" className="info-avatar"/>
+                            <span className="info-status"></span>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
