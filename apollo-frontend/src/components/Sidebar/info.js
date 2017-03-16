b45a185fb0131bf164dc7b1a46fe5bec7b740690
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-16 10:39:48 -0700

    Created seperate sidebar components

---
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SidebarInfo extends Component {
+    render() {
+        return (
+            <div className="sidebar-info">
+                Sidebar Info
+            </div>
+        );
+    }
+}
