f094e6b4663b712244569208e01dd1abbf370149
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-16 10:49:57 -0700

    Added content class name

--- apollo-frontend/src/components/Home/index.js
+++ apollo-frontend/src/components/Home/index.js
@@ -4,7 +4,7 @@ import { client } from '../../modules/api-client';
 export default class Home extends Component {
     render(){
         return (
-            <div>Home</div>    
+            <div className="content">Home</div>    
         )    
     }
 }
