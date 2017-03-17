e25d991fdfbebc1466bb3a8bf1fc2006babf72a6
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-16 19:15:06 -0700

    Created intital landing page

---
+++ apollo-frontend/src/components/Landing/index.js
@@ -0,0 +1,10 @@
+import React, { PropTypes, Component } from 'react'; 
+import { client } from '../../modules/api-client';
+
+export default class Landing extends Component {
+    render(){
+        return (
+            <div>Landing</div>    
+        )    
+    }
+}
