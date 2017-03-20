a93ead46db4ed57bb16b8e64774e2c4616595e6c
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-20 15:05:35 -0700

    Reenabled the database connection

--- apollo-backend/modules/database.js
+++ apollo-backend/modules/database.js
@@ -1,14 +1,14 @@
 var mysql = require('promise-mysql');
 var config = require('../config/config');
 
-// var pool = mysql.createPool({
-//     host: config['APOLLO_MYSQL_IP'],
-//     user: config['APOLLO_MYSQL_USER'],
-//     password: config['APOLLO_MYSQL_PASS'],
-//     port: config['APOLLO_MYSQL_PORT'],
-//     database: 'apollo',
-//     connectionLimit: 200,
-//     supportBigNumbers: true
-// });
-//
-// exports.pool = pool;
+var pool = mysql.createPool({
+    host: config['APOLLO_MYSQL_IP'],
+    user: config['APOLLO_MYSQL_USER'],
+    password: config['APOLLO_MYSQL_PASS'],
+    port: config['APOLLO_MYSQL_PORT'],
+    database: 'apollo',
+    connectionLimit: 200,
+    supportBigNumbers: true
+});
+
+exports.pool = pool;
