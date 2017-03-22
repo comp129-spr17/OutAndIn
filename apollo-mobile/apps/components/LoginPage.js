908c5241b392899923e138208f54c8e99ed410ea
maxine139 <m_lien@u.pacific.edu>
2017-03-21 23:20:58 -0700

    added navigating properties to the login page and the main chat page. need to fix undefined object error??

--- apollo-mobile/apps/components/LoginPage.js
+++ apollo-mobile/apps/components/LoginPage.js
@@ -1,22 +1,35 @@
 'use strict'
 
 import React, { Component } from 'react';
-import { StyleSheet, AppRegistry, Text, ListView, TextInput, Image, TouchableOpacity, View, ScrollView } from 'react-native';
+import { StyleSheet, AppRegistry, Text, ListView, TextInput, Image, Navigator, TouchableOpacity, View, ScrollView } from 'react-native';
 
 export default class LoginPage extends Component
 {
+  _handlePress()
+  {
+    this.props.navigator.push({id: 'mainChat'})
+  }
+
   render(){
     return(
       <Image source={require('../../Img/landing.jpg')} style = {styles.container}>
-      <View>
-        <Text style = {styles.textLabel}>Username or Email</Text>
-        <TextInput style = {styles.textInput}/>
-      </View>
+        <View style = {styles.container}>
+          <Image source={require('../../Img/apollo-logo.png')}/>
+          <Text style = {styles.textLabel}>Messaging Made Easy</Text>
+          <View>
+            <TouchableOpacity onPress = {this._handlePress}>
+              <Text style = {styles.textLabel}>Login</Text>
+            </TouchableOpacity>
+            <TextInput style = {styles.textInput}/>
+          </View>
 
-      <View>
-        <Text style = {styles.textLabel}>Password</Text>
-        <TextInput style = {styles.textInput}/>
-      </View>
+          <View>
+            <TouchableOpacity>
+              <Text style = {styles.textLabel}>Register</Text>
+            </TouchableOpacity>
+            <TextInput style = {styles.textInput}/>
+          </View>
+        </View>
       </Image>
 
     )
@@ -27,17 +40,18 @@ const styles = StyleSheet.create({
   container:
   {
     flex: 1,
-    
+    justifyContent: 'center',
+    alignItems: 'center',
     width: null,
     height: null,
-    resizeMode: 'stretch'
   },
 
   textLabel:
   {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    backgroundColor: 'rgba(0,0,0,0)',
+    color: 'white'
   },
 
   textInput:
