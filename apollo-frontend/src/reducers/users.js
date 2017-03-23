ba987cd209a78ba23a1a4837859414f10f48afe6
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-23 13:53:06 -0700

    Deleted old actions and reducers

--- apollo-frontend/src/reducers/users.js
+++
@@ -1,9 +0,0 @@
-export function users(state={}, action){
-    console.log("action :" + JSON.stringify(action));
-    switch(action.type){
-        case 'USERS_CREATE_SUCCESS':
-            return action.username;
-        default:
-            return state;
-    }
-}
