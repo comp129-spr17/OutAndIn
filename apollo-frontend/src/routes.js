61b76c315f8ec5c5f58283f2c3423efd6b102804
MalvikaSriram <m_sriram@u.pacific.edu>
2017-03-25 10:08:55 -0700

    Route and component for search users with code from sidebar main.js

--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -10,8 +10,10 @@ import NotFound from './components/NotFound';
 import Blank from './components/Layouts/blank';
 import Main from './components/Layouts/main';
 import Profile from './components/Profile';
+import UserSearch from './components/UserSearch';
 import ReduxTest from './containers/user';
 
+ 
 
 export default (
     <Router history={browserHistory} >
@@ -21,6 +23,7 @@ export default (
             <Route path="/chat" component={Chat} />
             <Route path="/sockTest" component={SockTest} />
             <Route path="/profile" component={Profile} />
+            <Route path="/usersearch" component={UserSearch} />
         </Route>
         <Route component={Blank} >
             <Route path="/redux" component={ReduxTest} />
