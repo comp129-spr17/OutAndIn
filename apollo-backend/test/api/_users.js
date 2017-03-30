21b245ba131185e6acefc7f6e787074b64524f58
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-30 14:56:14 -0700

    Added rest api testing framework and example

---
+++ apollo-backend/test/api/_users.js
@@ -0,0 +1,22 @@
+
+var testsRunningInNode = (typeof global !== "undefined" ? true : false),
+    chakram = (testsRunningInNode ? global.chakram : window.chakram),
+    expect = (testsRunningInNode ? global.expect : window.expect);
+
+describe('API: Users Resource', function(){
+    it('GET: Should return a list of users', function(){
+        var response = chakram.get('http://localhost:4200/api/v1/users');
+        expect(response).to.have.json('header.object', "user");
+        expect(response).to.have.json('body.list', []);
+        expect(response).to.have.status(200)
+        return chakram.wait();
+
+        /*
+        var missing = chakram.delete('http://localhost:4200/api/v1/users');
+        return chakram.waitFor([
+            expect(missing).to.have.status(404),
+            expect(response).to.have.status(200)
+        ]);
+        */
+    });
+});
