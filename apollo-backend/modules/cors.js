e56ac88822e29042ac930a91209820c853fb773a
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-03-30 17:59:32 -0700

    Restoring CORS, need to fix this later

--- apollo-backend/modules/cors.js
+++ apollo-backend/modules/cors.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Request-Headers", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     
