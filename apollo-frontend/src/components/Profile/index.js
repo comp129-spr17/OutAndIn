45ba19066bb188cc56acfef1396cfbbe25b041c4
MalvikaSriram <m_sriram@u.pacific.edu>
2017-04-01 12:15:08 -0700

    tried some css styling for profile but it is not working right now. Ill fix it tonight.

--- apollo-frontend/src/components/Profile/index.js
+++ apollo-frontend/src/components/Profile/index.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-
-export default class Profile extends React.Component {
-    constructor() {
+export default class Friends extends Component {
+    constructor () { 
         super();
-        
-        this.state= { 
-	 		username: '',
-	 		password: ''
-        };
-	    this.handleUserNameChange = this.handleUserNameChange.bind(this);
-	    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.state={
+            user:{
+                name:'Abc',
+                handle:'@abc',
+                avatar:'/img/avatar1.jpg',
+                phoneNumber:'+1-2345678',
+                dob:'12-01-2011',
+                address:'Here and There',
+                friends:['/img/avatar1.jpg','/img/avatar2.jpg','/img/avatar3.jpg']
+            }
+        }
     }
-    
-     handleUserNameChange(event){
-        this.setState({username: event.target.username}) 
-        this.setState({password: event.target.password})
-    }
-    
-     handlePasswordChange(event){
-        this.setState({password: event.target.password})
-    }
-    
-    handleSignUp(event){  
-		event.preventDefault();
-    }
-    
-    
+
     render() {
         return (
-            <div className="login">
-                <form className='loginForm'>
-                    <input autoFocus type="text" value={this.state.username} onChange={this.handleUserNameChange} autoComplete="off"  placeholder='Enter UserName'/>
-                    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} autoComplete="off"  placeholder='Enter Password'/>
-                    <button onClick={this.handleSignUp}>Submit</button>
-                </form>
-            
-            
+            <div> 
+                <div >
+                    <div className='profile-avatar'>
+                        <img  src={ this.state.user.avatar } alt="User Image"/>
+                    </div>
+                    <div  >
+                        <p className='profile-name' >{this.state.user.name} </p>
+                        <p className='profile-handle' > {this.state.user.handle} </p>
+                    </div>
+                </div>
+                <div className='profile-otherinfo'>
+                    <p> Phone : {this.state.user.phoneNumber}  </p>
+                    <p> BirthDay : {this.state.user.dob}  </p>
+                    <p> Where I Live : {this.state.user.address}  </p>
+                    <div>
+                        <p>Friends </p>
+                        <div className='profile-friendlist'> 
+                             { this.state.user.friends.map((friend, k) => {
+                                return <img  src={ friend }  key={k} alt="User Image"/>
+                             })}
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
     }
-
 }
