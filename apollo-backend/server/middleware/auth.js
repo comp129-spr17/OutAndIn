a84716989f1b2a1b68fcb3ec86db79015fb3650e
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-02 22:34:23 -0700

    Created authentication middleware. Works, but disabled for now

--- apollo-backend/server/middleware/auth.js
+++ apollo-backend/server/middleware/auth.js
@@ -1,34 +1,38 @@
+var jwt = require('jsonwebtoken');
+var ResponsePayload = require('../../controllers/controller');
+
 module.exports = function(req, res, next) {
     // Get token from Authorization header
-    var token = req.headers["authorization"];
+	var token = req.headers["authorization"];
     if (!token){
         // Token does not exist. User must login or register
-        return res.status(401).json({
-            header: {
-                object: 'AUTH',
-                action: 'authenticate',
-                code: 0,
-                message: "User is not authenticated"
-            },
-            body: {}
-        });
-    }
+		var response = new ResponsePayload();
+		response.setStatus(401);
+		response.setCount(1);
+		response.setType("authorization");
+		response.pushResult({
+			"code": "InsufficientPermissions",
+			"message": "User is not authenticated",
+			"objectName": "authentication"
+		});
+		return res.status(401).json(response.getResponse());
+	}
 
     // Parse token
     token = token.replace('Bearer ', '');
-    
     // Verify the token
-    jwt.verify(token, "apollo-secret", function(err, user){
-        if(err){
-            return res.status(401).json({
-                header: {
-                    object: 'AUTH',
-                    action: 'authenticate',
-                    code: 0,
-                    message: "Token is invalid. Authenicated failed."
-                },
-                body: {}                
-            });
+	jwt.verify(token, "super-secret", function(err, user){
+		if(err){
+			var response = new ResponsePayload();
+			response.setStatus(401);
+			response.setCount(1);
+			response.setType("authorization");
+			response.pushResult({
+				"code": "InsufficientPermissions",
+				"message": "Token is invalid",
+				"objectName": "authentication"
+			});
+			return res.status(401).json(response.getResponse());
         } else {
             req.user = user;
             next();
