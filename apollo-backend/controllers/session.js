58f0d556f9f5a135844965336aa6ebe8571d8400
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-02 20:29:49 -0700

    Not complete. File will handle sessions

---
+++ apollo-backend/controllers/session.js
@@ -0,0 +1,70 @@
+/**
+ *  @(Project): Apollo Backend
+ *  @(Filename): session.js
+ *  @(Description): Session route handlers
+ *  @(Authors): Out-N-In Team
+ *  @(License): MIT
+ */
+
+var express = require('express');
+var router = express.Router();
+var usersService = require('../services/users');
+
+// OPTIONS - Routes permitted per route
+var _OPTIONS = {
+	// Default Users Route
+	"/": {
+		"METHODS": [
+			"POST",
+			"DELETE"
+		],
+		"HASHES": new Set()
+	}
+};
+
+function init(){
+	// Initialize the OPTIONS methods for each route
+	// Add all routes into the set
+	var keys = Object.keys(_OPTIONS);
+	keys.forEach(function(key){
+		var methods = _OPTIONS[key]["METHODS"];
+		methods.forEach(function(method){
+			_OPTIONS[key]["HASHES"].add(method);
+		});
+	});
+}
+
+router.post('/', function(req, res){
+	console.log(req.body);
+	res.sendStatus(200);
+});
+
+router.delete('/', function(req, res){
+	res.sendStatus(200);
+});
+
+router.options('/', function(req, res){
+	var origin = req.get('Origin');
+	// Check if origin is set
+	if(!origin){
+		res.sendStatus(404);		
+		return;
+	}
+	// Check if method that is requested is in the methods hash set
+	var method = req.get('Access-Control-Request-Method');
+	if(_OPTIONS["/"]["HASHES"].has(method)){
+		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+		res.header("Access-Control-Max-Age", 86400);
+		res.sendStatus(200);
+		return;
+	}
+	// Send 404 if both of the above conditions are not met
+	res.sendStatus(404);
+});
+
+module.exports = {
+	init: init,
+	router: router
+};
+
