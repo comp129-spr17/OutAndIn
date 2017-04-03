964b5962708c6c2ec6ceeed114d08e51f4b7e1c2
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-02 22:33:47 -0700

    Changed jwt package

--- apollo-backend/services/authentication.js
+++ apollo-backend/services/authentication.js
@@ -1,12 +1,12 @@
-var jwt = require('jwt-simple');
+var jwt = require('jsonwebtoken');
 var secret = "super-secret";
 
 exports.generateToken = function(uuid){
 	var payload = {
 		exp: Math.floor(new Date()/1000) + (24*60*60),
 		iat: Math.floor(new Date()/1000),
-		sub: uuid
+		sub: uuid[0]
 	};
-	return jwt.encode(payload, secret);
+	return jwt.sign(payload, secret);
 };
 
