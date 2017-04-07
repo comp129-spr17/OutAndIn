3074155e6ff70b470d47a316c934614227d6c0a7
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-06 21:52:29 -0700

    Updated responses for route handlers

--- apollo-backend/controllers/users.js
+++ apollo-backend/controllers/users.js
@@ -1,21 +1,19 @@
 'use strict';
 /**
- *  @(Project): Apollo Backend
- *  @(Filename): users.js
- *  @(Description): Users route handlers
- *  @(Authors): Out-N-In Team
- *  @(License): MIT
+ *  Apollo
+ *  @description: Users route handlers
+ *  @author: Out-N-In Team
+ *  @license: MIT
  */
 
 var express = require('express');
 var router = express.Router();
 var usersService = require('../services/users');
 var chatsService = require('../services/chats');
-var ResponsePayload = require('./controller');
+var responseObject = require('./controller').responseObject;
 
-// OPTIONS - Routes permitted per route
+// Routes permitted per route
 var _OPTIONS = {
-	// Default Users Route
 	"/": {
 		"METHODS": [
 			"GET",
@@ -31,6 +29,13 @@ var _OPTIONS = {
 	}
 };
 
+/**
+ * Init
+ * @constructor
+ * @description: Initialize OPTIONS methods for each route
+ * @param: {none}
+ * @return: {none}
+ */
 function init(){
 	// Initialize the OPTIONS methods for each route
 	// Add all routes into the set
@@ -43,93 +48,154 @@ function init(){
 	});
 }
 
+/**
+ * GET - ["/"]
+ * @description: Get all users
+ * @param: {none}
+ * @return: {object} users - Collection of users
+ */
 router.get('/', function(req, res){
-    usersService.getUserIDList().then((users) => {
-		res.json({
-			header: {
-				'object': 'user',
-				'action': 'list',
-				'code': 0,
-				'message': 'success'
-			},
-			body: {
-				list:users
-			}
-		});
+	usersService.getAllUsers().then((users) => {
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setResults(users);
+		// Send response
+		res.status(200).json(response.toJSON());
 	}).catch((err) => {
-		res.json({
-			header: {
-				'object': 'user',
-				'action': 'list',
-				'code': 1,
-				'message': 'error'
-			},
-			body: {
-				error: err
-			}
-		});
+		// Error object
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		response.setErrors(error);
+		// Send Response
+		res.status(500).json(response.toJSON());
 	});
 });
 
-//create user
+/**
+ * POST - ["/"]
+ * @description: Create user
+ * @param: {string} username - Username of the user
+ * @return: {object} user - Details of the newly created user
+ */
 router.post('/', function(req, res){
-	usersService.getUserByUsername(req.body.username).then((users) =>{
+	var username = req.body.username;
+	usersService.getUserByUsername(username).then((users) => {
         // User already exists
-        if(users.length != 0){
-            return 1;
+		if(users.length != 0){
+			// Error
+			var error = {
+				"code": 1000,
+				"message": "User already exists"
+			};
+			// Create response object
+			let response = new responseObject();
+			// Set attributes
+			response.setSuccess(false);
+			response.setErrors(error);
+			// Send response
+			res.status(401).json(response.toJSON());
+            return false;
         }
-        // Doesn't exist, create it
-        return usersService.createUser("", req.body.username, "", "");
+        // User does not exist, create the user
+        return usersService.createUser("", username, "", "");
     }).then((data) => {
-        if(data == 1){
-			//return error, user already exists
-            return 1;
+        if(data == false){
+			// return error, user already exists
+            return false;
         }
-		return usersService.getUserByUsername(req.body.username);
-    }).then((users) =>{
-        if(users == 1) {
-            //return error, user already exists
-			res.json({
-				header: {
-					'object': 'user',
-					'action': 'create',
-					'code': 1,
-					'message': 'User already exists'
-				},
-				body: {
-					id: -1
-				}
-			});
-            return;
-        }
-		var response = {
-			header: {
-				'object': 'user',
-				'action': 'create',
-				'code': 0,
-				'message': 'success'
-			},
-			body: {
-				id: users[0].uuid
-			}
-        };
-        res.io.sockets.emit('usersConnected', {});
-		res.json(response);
-    }).catch((err) => {
-		res.json({
-			header: {
-				'object': 'user',
-				'action': 'create',
-				'code': 2,
-				'message': 'error'
-			},
-			body: {
-				error: err
-			}
-		});
+		return usersService.getUserByUsername(username);
+    }).then((user) =>{
+		if(user == false) {
+            return false;
+		}
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setResults(user);
+		// Emit socket event
+		res.io.sockets.emit('usersConnected', {});
+		// Send response
+		res.status(200).json(response.toJSON());
+	}).catch((err) => {
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setErrors(error);
+		// Emit socket event
+		res.io.sockets.emit('usersConnected', {});
+		// Send response
+		res.status(500).json(response.toJSON());
     });
 });
 
+/**
+ * GET - ["/:uuid"]
+ * @description: Get user information by UUID
+ * @param: {string} uuid - UUID of the user
+ * @return: {object} user - Details of the user
+ */
+router.get('/:uuid', function(req, res){
+	var uuid = req.params.uuid;
+	usersService.getUserByUUID(uuid).then((user) => {
+		// User does not exist
+		if(user.length == 0){
+			// Error
+			var error = {
+				"code": 1000,
+				"message": "User does not exist"	
+			};
+			// Create response object
+			let response = new responseObject();
+			// Set attributes
+			response.setSuccess(false);
+			response.setErrors(error);
+			// Send response
+			res.status(401).json(response.toJSON());
+			return;
+		}
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setResults(user);
+		// Send response
+		res.status(200).json(response.toJSON());
+	}).catch((err) => {
+		// Error
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		response.setErrors(error);
+		// Send response
+		res.status(500).json(response.toJSON());
+    });
+});
+
+/**
+ * OPTIONS - ["/"]
+ * @description: Responds to authorized headers and methods request
+ * @param: {none}
+ * @return: {string} http headers - Authorized headers and methods
+ */
 router.options('/', function(req, res){
 	var origin = req.get('Origin');
 	// Check if origin is set
@@ -140,7 +206,6 @@ router.options('/', function(req, res){
 	// Check if method that is requested is in the methods hash set
 	var method = req.get('Access-Control-Request-Method');
 	if(_OPTIONS["/"]["HASHES"].has(method)){
-
 		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
 		res.header("Access-Control-Max-Age", 86400);
@@ -151,22 +216,42 @@ router.options('/', function(req, res){
 	res.sendStatus(404);
 });
 
-// Get basic info about the authenticated user
+/**
+ * GET - ["/me"]
+ * @description: Get the users own information
+ * @param: {none}
+ * @return: {object} user - User object
+ */
 router.get('/me', function(req, res){
+	var user = req.user;
 	// Get User by UUID
-	usersService.getUserByUUID(req.user).then((user) => {
-		let response = new ResponsePayload();
-		response.setStatus("success");
-		response.setCount(1);
-		response.setType("users");
-		response.pushResult(user[0]);
-		res.status(200).json(response.getResponse());
+	usersService.getUserByUUID(user).then((user_info) => {
+		// Create response object
+		let response = new responseObject();
+		response.setSuccess(true);
+		response.setResults(user_info);
+		// Send JSON of the response object
+		res.status(200).json(response.toJSON());
 	}).catch((err) => {
-		console.log(err);
-		res.status(500).send("system error");
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setErrors(error);
+		res.status(500).json(response.toJSON());
 	});
 });
 
+/**
+ * OPTIONS - ["/"]
+ * @description: Responds to authorized headers and methods request
+ * @param: {none}
+ * @return: {string} http headers - Authorized headers and methods
+ */
 router.options('/me', function(req, res){
 	var origin = req.get('Origin');
 	// Check if origin is set
@@ -186,94 +271,6 @@ router.options('/me', function(req, res){
 	}
 	// Send 404 if both of the above conditions are not met
 	res.sendStatus(404);
-});
-// Create User Manually
-router.get('/setup', function(req, res){
-    var username = "apollo";
-    var password = "apollo";
-
-    usersService.getUserByUsername(username).then((users) =>{
-        // User already exists
-        if(users.length != 0){
-            return 1;
-        }
-        // Doesn't exist, create it
-        return usersService.createUser("apollo@apollo.com", "apollo", "apollo", "apollo");
-    }).then((data) => {
-        if(data == 1){
-            res.send("User already exists");
-            return;
-        }
-        res.send("User created successfully");
-    }).catch((err) => {
-        res.send("Error: " + err);
-    });
-});
-
-//get all user ids
-router.get('/list', function(req, res){
-	usersService.getUserIDList().then((users) => {
-		res.json({
-			header: {
-				'object': 'user',
-				'action': 'list',
-				'code': 0,
-				'message': 'success'
-			},
-			body: {
-				list:users
-			}
-		});
-		return;
-	}).catch((err) => {
-		res.json({
-			header: {
-				'object': 'user',
-				'action': 'list',
-				'code': 1,
-				'message': 'error'
-			},
-			body: {
-				error: err
-			}
-		});
-	});
-});
-
-//get user data by id
-router.get('/id/:id', function(req, res){
-	usersService.getUserByUUID(req.params.id).then((users) =>{
-		var response = {
-			header: {
-				'object': 'user',
-				'action': 'details',
-				'code': 1,
-				'message': 'error'
-			},
-            body: {}
-		};
-		if(users.length != 1){
-			//error
-			return res.json(response);
-		}
-		//success
-		response.header.code = 0;
-		response.header.message = 'success';
-		response.body.user = users[0];
-		res.send(response);
-    }).catch((err) => {
- 		res.json({
-			header: {
-				'object': 'user',
-				'action': 'details',
-				'code': 2,
-				'message': 'error'
-			},
-			body: {
-				error: err
-			}
-		});
-    });
 });
 
 module.exports = {
