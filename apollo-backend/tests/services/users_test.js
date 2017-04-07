e93ed36aecc1909d0e0fc18b5d2e7b57a89e3e55
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-07 01:20:24 -0700

    Finished user services test cases

--- apollo-backend/tests/services/users_test.js
+++ apollo-backend/tests/services/users_test.js
@@ -4,22 +4,24 @@ var db = require('../../modules/database');
 describe("[Users]", function(){
 	beforeEach(function(){
 		// Delete user just in case its in their
-		var uuid = "test-id";
-		var sql1 = "DELETE FROM users WHERE uuid = ?";
+		var sql1 = "DELETE FROM users WHERE username = ?";
 		var user = {
-			"uuid": uuid,
+			"uuid": "test-id",
 			"email": "test@test.com",
 			"fullname": "test user",
 			"username": "test",
-			"password": "password"
+			"password": "password",
+			"avatar": "http://example.com/path/to/avatar.jpg",
+			"socket": "socket-id"
 		};
 		// Create the user
-		var sql2 = "INSERT INTO users VALUES ('', ?, ?, ?, ?, ?, '', '')";
-		return db.pool.query(sql1, [uuid]).then((data) => {
+		var username = "test";
+		var sql2 = "INSERT INTO users VALUES ('', ?, ?, ?, ?, ?, ?, ?)";
+		return db.pool.query(sql1, [username]).then((data) => {
 			if(!data.hasOwnProperty("affectedRows")){
 				throw new Error("Deleting the test user failed");	
 			}
-			return db.pool.query(sql2, [user.uuid, user.email, user.fullname, user.username, user.password]);
+			return db.pool.query(sql2, [user.uuid, user.email, user.fullname, user.username, user.password, user.avatar, user.socket]);
 		}).then((data) => {
 			if(!data.hasOwnProperty("affectedRows")){
 				throw new Error("Creating the test user failed");	
@@ -45,6 +47,84 @@ describe("[Users]", function(){
 			var email = "test@test.com";
 			return usersService.getUserByEmail(email).then((user) => {
 				expect(user[0]).to.have.all.keys("uuid", "email", "fullname", "username", "avatar");
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("getAllUsers", function(){
+		it("Should return a collection of users", function(){
+			return usersService.getAllUsers().then((users) => {
+				expect(users.length).to.be.above(1);
+				expect(users[0]).to.have.all.keys("uuid", "email", "fullname", "username", "avatar");
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("getUserByUUID", function(){
+		it("Should return a user", function(){
+			var uuid = "test-id";
+			return usersService.getUserByUUID(uuid).then((user) => {
+				expect(user[0]).to.have.all.keys("uuid", "email", "fullname", "username", "avatar");
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("getSocketID", function(){
+		it("Should return a user", function(){
+			var uuid = "test-id";
+			return usersService.getSocketID(uuid).then((user) => {
+				expect(user[0]).to.have.all.keys("socket");
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("createUser", function(){
+		it("Should create a new user", function(){
+			var user = {
+				"email": "test@test.com",
+				"fullname": "test user",
+				"username": "test",
+				"password": "password"
+			};
+			return usersService.createUser(user.email, user.username, user.fullname, user.password).then((data) => {
+				if(!data.hasOwnProperty("affectedRows")){
+					throw new Error("Creating the test user failed");	
+				}
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("storeSocketID", function(){
+		it("Should store a socket ID", function(){
+			var uuid = "test-id";
+			var socketID = "socket-id";
+			return usersService.storeSocketID(uuid, socketID).then((data) => {
+				if(!data.hasOwnProperty("affectedRows")){
+					throw new Error("Creating the test user failed");	
+				}
+			}).catch((err) => {
+				throw new Error(err);	
+			});
+		});
+	});
+
+	describe("deleteuser", function(){
+		it("Should delete a user", function(){
+			var uuid = "test-id";
+			return usersService.deleteUser(uuid).then((data) => {
+				if(!data.hasOwnProperty("affectedRows")){
+					throw new Error("Creating the test user failed");	
+				}
 			}).catch((err) => {
 				throw new Error(err);	
 			});
