e7d358e4e68954ae6206727f6f156d788a201000
Cynthia <c_phan4@u.pacific.edu>
2017-04-09 13:18:39 -0700

    working on static representations of the info sidebar content

--- apollo-frontend/src/components/Sidebar/info.js
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -8,6 +8,7 @@ export default class SidebarInfo extends Component {
 
                 <div className="sidebar-info-people">
                 People
+
                 <FontAwesome
                 className="sidebar-info-addbutton"
                 name='plus-circle'
@@ -16,14 +17,26 @@ export default class SidebarInfo extends Component {
 
                 <div className="sidebar-info-files">
                 Files
+
                 <FontAwesome
                 className="sidebar-info-addbutton"
                 name='plus-circle'
+                size='2x'/>
+
+                <FontAwesome
+                className="sidebar-info-fileicon"
+                name= 'file-audio-o'
+                size='2x'/>
+
+                <FontAwesome
+                className="sidebar-info-fileicon"
+                name='file-video-o'
                 size='2x'/>
                 </div>
 
                 <div className="sidebar-info-recent">
                 Recent Images & Video
+
                 <FontAwesome
                 className="sidebar-info-addbutton"
                 name='plus-circle'
