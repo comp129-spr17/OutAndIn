76a61f6d9b4700cc9042ccbe5af87110ea53c99a
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-09 07:12:20 -0700

    Currently working on this file

---
+++ apollo-backend/tests/controllers/chats_test.js
@@ -0,0 +1,9 @@
+describe("[Chats]", function(){
+	describe("[router]", function(){
+		describe("GET - ['/']", function(){
+			it("Should return a collection of chats for the user", function(){
+				expect("TEST").to.be.a('string');
+			});
+		});
+	});
+});
