5333cd26a67377c88046c57cfa3debb3c5e3c500
Cynthia <c_phan4@u.pacific.edu>
2017-04-09 01:23:08 -0700

    trying to link files

--- apollo-frontend/src/components/Sidebar/info.js
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class SidebarInfo extends Component {
     render() {
         return (
-            <div className="sidebar-info">
+            <div className="sidebar-info-main">
                 Sidebar Info
             </div>
         );
