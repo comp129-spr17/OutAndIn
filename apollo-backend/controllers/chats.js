d15a98b870101ad3e9112572c8dff8e683d7cc22
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-09 07:09:52 -0700

    Not finished. Pushing to store current stage in changes

--- apollo-backend/controllers/chats.js
+++ apollo-backend/controllers/chats.js
@@ -10,28 +10,17 @@ var router = express.Router();
 var chatsService = require('../services/chats');
 var usersService = require('../services/users');
 var responseObject = require('./controller').responseObject;
-
+var Promise = require('bluebird');
 // Routes permitted per route
 var _OPTIONS = {
 	"/": {
 		"METHODS": [
-			"POST",
-		],
-		"HASHES": new Set()
-	},
-	"/create": {
-		"METHODS": [
+			"GET",
 			"POST"
 		],
 		"HASHES": new Set()
 	},
-	"/id": {
-		"METHODS": [
-			"GET"
-		],
-		"HASHES": new Set()
-	},
-	"/addUser": {
+	"/:chatID/users": {
 		"METHODS": [
 			"POST"
 		],
@@ -73,13 +62,60 @@ function init(){
 
 /**
  * GET - ["/"]
+ * @description: Get all chats a user is a member of
+ * @param: {none} 
+ * @return: {array} chatID's - Collection of chats with their details
+ */
+router.get('/', function(req, res){
+	var userID = req.user;
+	chatsService.getChatsForUser(userID).then((chats) => {
+		var promises = [];
+		for(var chat in chats){
+			var chatID = chats[chat]["uuid"];
+			promises.push(chatsService.getUsersFromChat(chatID));
+		}
+		var returnChats = new Promise((resolve, reject) => {
+			resolve(chats);	
+		});
+		promises.push(returnChats);
+		return Promise.all(promises);
+	}).then((results) => {
+		// Store the chats from previous promise
+		var chats = results[results.length-1];
+		// Remove the chats from the users results
+		results.pop();
+		// Put users into chat objects
+		for(var i in chats){
+			chats[i]["users"] = [];
+			chats[i]["users"].push(results[i]);
+		}
+		let response = responseObject();
+		response.setSuccess(200);
+		response.setResults(chats);
+		res.status(200).json(response.toJSON());
+	}).catch((err) => {
+		var error = {
+			"code": 1000,
+			"message": "System error"
+		};
+		let response = new responseObject();
+		response.setSuccess(false);
+		response.setErrors(error);
+		res.status(500).json(response.toJSON());
+	});
+});
+
+/**
+ * POST - ["/"]
  * @description: Create chat
  * @param: {none}
- * @return: {object} users - Collection of users
+ * @return: {string} chatID - UUID of the chat
  */
 router.post('/', function(req, res){
+	var userID = "1234";
+	var friendID = req.body.friendID;
 	var chatID = chatsService.generateChatID();
-	chatsService.createChat(chatID).then((chat) => {
+	chatsService.createChat(chatID, userID, friendID).then((chat) => {
 		// Create response object
 		let response = new responseObject();
 		// Set attributes
@@ -87,29 +123,77 @@ router.post('/', function(req, res){
 		response.setResults(chat);
 		// Send response
 		res.status(200).json(response.toJSON());
-
-		res.json({
-			header:{
-				code: 0,
-				message: 'success'
-			},
-			body: {
-				chatID: chatID
-			}
-		});
-	}).catch((err) =>{
-		res.json({
-			header:{
-				code: 1,
-				message: 'ERROR: sql'
-			},
-			body:{
-				err : err
-			}
-		});
+	}).catch((err) => {
+		// Error
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		response.setErrors(error);
+		// Send response
+		res.status(500).json(response.toJSON());
 	});
 });
 
+/**
+ * GET - ["/:chatID"]
+ * @description: Get chat details
+ * @param: {none}
+ * @return: {string} chatID - UUID of the chat
+ */
+router.get('/:chatID', function(req, res){
+	// TODO:(mcervco) Check whether the user is a part of the chat
+	// Don't want users to just get chat info about any chat
+	var uuid = req.params.chatID;
+	chatsService.getChatByUUID(uuid).then((chat) =>{
+		if(chat.length == 0){
+			// Error
+			var error = {
+				"code": 1000,
+				"message": "Chat does not exist"
+			};
+			// Create response object
+			let response = new responseObject();
+			// Set attributes
+			response.setSuccess(false);
+			response.setErrors(error);
+			// Send response
+			res.status(401).json(response.toJSON());
+			return;
+		}
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(true);
+		response.setResults(chat);
+		// Send response
+		res.status(200).json(response.toJSON());
+	}).catch((err) => {
+		// Error
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		response.setErrors(error);
+		// Send response
+		res.status(500).json(response.toJSON());
+	});
+});
+
+/**
+ * OPTIONS - ["/"]
+ * @description: Responds to authorized headers and methods request
+ * @param: {none}
+ * @return: {string} http headers - Authorized headers and methods
+ */
 router.options('/', function(req, res){
 	var origin = req.get('Origin');
 	// Check if origin is set
@@ -121,6 +205,108 @@ router.options('/', function(req, res){
 	var method = req.get('Access-Control-Request-Method');
 	if(_OPTIONS["/"]["HASHES"].has(method)){
 
+		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+		res.header("Access-Control-Max-Age", 86400);
+		res.sendStatus(200);
+		return;
+	}
+	// Send 404 if both of the above conditions are not met
+	res.sendStatus(404);
+});
+
+/**
+ * POST - ["/:chatID/users"]
+ * @description: Add user to chat
+ * @param: {none}
+ * @return: {string} chatID - UUID of the chat
+ */
+router.post("/:chatID/users", function(req,res){
+	var chatID = req.params.chatID;
+	var userID = req.body.userID;
+	chatsService.getUserFromChat(chatID, userID).then((user) => {
+		if(user.length != 0){
+			// Error
+			var error = {
+				"code": 1000,
+				"message": "User already exists in the chat"
+			};
+			// Create response object
+			let response = new responseObject();
+			// Set attributes
+			response.setSuccess(false);
+			response.setErrors(error);
+			// Send response
+			res.status(401).json(response.toJSON());
+			return false;
+		}
+		return chatsService.addUserToChat(chatID, userID);
+	}).then((chat) => {
+		// Handle previously completed promises
+		if(chat == false){
+			return false;
+		}
+		return usersService.getSocketID(userID);
+	}).then((socketID) => {
+		// Handle previously completed promises
+		if(socketID == false){
+			return false;
+		}
+		if(!res.io.sockets.connected[sock[0].socket]){
+			// Error
+			var error = {
+				"code": 1000,
+				"message": "User socket does not exists"
+			};
+			// Create response object
+			let response = new responseObject();
+			// Set attributes
+			response.setSuccess(false);
+			response.setErrors(error);
+			// Send response
+			res.status(401).json(response.toJSON());
+			return;
+		}
+		res.io.sockets.connected[sock[0].socket].emit('chatsListUpdate', {});
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		// Send response
+		res.status(200).json(response.toJSON());
+	}).catch((err) => {
+		// Error
+		var error = {
+			"code": 1000,
+			"message": err
+		};
+		// Create response object
+		let response = new responseObject();
+		// Set attributes
+		response.setSuccess(false);
+		response.setErrors(error);
+		// Send response
+		res.status(500).json(response.toJSON());
+	});
+});
+
+/**
+ * OPTIONS - ["/:chatID/users"]
+ * @description: Responds to authorized headers and methods request
+ * @param: {none}
+ * @return: {string} http headers - Authorized headers and methods
+ */
+router.options('/:chatID/users', function(req, res){
+	var origin = req.get('Origin');
+	// Check if origin is set
+	if(!origin){
+		res.sendStatus(404);
+		return;
+	}
+	// Check if method that is requested is in the methods hash set
+	var method = req.get('Access-Control-Request-Method');
+	if(_OPTIONS["/:chatID/users"]["HASHES"].has(method)){
+
 		res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
 		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
 		res.header("Access-Control-Max-Age", 86400);
@@ -130,302 +316,6 @@ router.options('/', function(req, res){
 	// Send 404 if both of the above conditions are not met
 	res.sendStatus(404);
 });
-
-
-//create chat
-/*
-output:
-{
-	chatID: #
-}
-*/
-router.post('/create', function(req, res){
-	var chatID = chatsService.chatsGenID();
-	chatsService.chatsCreateChat(chatID).then((chat) => {
-		console.log("CHAT: " + JSON.stringify(chat));
-		res.json({
-			header:{
-				code: 0,
-				message: 'success'
-			},
-			body: {
-				chatID: chatID
-			}
-		});
-	}).catch((err) =>{
-		res.json({
-			header:{
-				code: 1,
-				message: 'ERROR: sql'
-			},
-			body:{
-				err : err
-			}
-		});
-	});
-});
-
-
-router.options('/create', function(req, res){
-	var origin = req.get('Origin');
-	// Check if origin is set
-	if(!origin){
-		console.log('no origin');
-		res.sendStatus(404);
-		return;
-	}
-	// Check if method that is requested is in the methods hash set
-	var method = req.get('Access-Control-Request-Method');
-	if(_OPTIONS["/create"]["HASHES"].has(method)){
-
-		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-		res.header("Access-Control-Max-Age", 86400);
-		res.sendStatus(200);
-		return;
-	}
-
-	// Send 404 if both of the above conditions are not met
-	console.log('no method');
-	res.sendStatus(404);
-});
-
-//get chat details
-/*
-input:
-{
-	id: #
-}
-*/
-router.get('/id/:id', function(req, res){
-	chatsService.chatsGetChatByID(req.params.id).then((chats) =>{
-		if(chats.length == 0){
-			//no chats found
-			res.json({
-				header:{
-					code: 1,
-					message: 'No chat found'
-				},
-				body:{
-					id : req.params.id
-				}
-			});
-		}else if(chats.length > 1){
-			res.json({
-				header:{
-					code: 2,
-					message: 'more than one chat'
-				},
-				body:{
-					chats: chats,
-					id : req.params.id
-				}
-			});
-		}else{
-			res.json({
-				header:{
-					code: 0,
-					message: 'success'
-				},
-				body:{
-					chat : chats[0]
-				}
-			});
-		}
-	}).catch((err) =>{
-		res.json({
-			header:{
-				code: 3,
-				message: 'ERROR: sql'
-			},
-			body:{
-				err : err
-			}
-		});
-	});
-});
-
-
-router.options('/id', function(req, res){
-	var origin = req.get('Origin');
-	// Check if origin is set
-	if(!origin){
-		console.log('no origin');
-		res.sendStatus(404);
-		return;
-	}
-	// Check if method that is requested is in the methods hash set
-	var method = req.get('Access-Control-Request-Method');
-	if(_OPTIONS["/id"]["HASHES"].has(method)){
-
-		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-		res.header("Access-Control-Max-Age", 86400);
-		res.sendStatus(200);
-		return;
-	}
-
-	// Send 404 if both of the above conditions are not met
-	res.sendStatus(404);
-});
-
-
-//add user to chat
-/*
-input:
-{
-	chatID: #,
-	userID: #
-}
-*/
-router.post("/addUser", function(req,res){
-	chatsService.chatsGetChatUser(req.body.chatID, req.body.userID).then((chat_user)=>{
-		if(chat_user.length == 0){
-			//no connection yet
-			chatsService.chatsAddUserToChat(req.body.chatID, req.body.userID).then((chat) => {
-				//TODO: push above ^
-				//send socket event
-				usersService.getSocketID(req.body.userID).then((sock) => {
-					if(res.io.sockets.connected[sock[0].socket]){
-						//fine
-						res.io.sockets.connected[sock[0].socket].emit('chatsListUpdate', {});
-					}else{
-						res.json({
-							header:{
-								code: 3,
-								message: 'ERROR: socket not connected'
-							},
-							body:{
-								sock: sock
-							}
-						});
-					}
-					res.json({
-						header:{
-							code: 0,
-							message: 'success'
-						},
-						body: {}
-					});
-				}).catch((err) =>{
-					res.json({
-						header:{
-							code: 2,
-							message: 'ERROR: sql, could not get socket id'
-						},
-						body:{
-							err : err
-						}
-					});
-				});
-
-			}).catch((err) =>{
-				res.json({
-					header:{
-						code: 1,
-						message: 'ERROR: sql could not make entry'
-					},
-					body:{
-						err : err
-					}
-				});
-			});
-		}else{
-			//connection already exists
-			res.json({
-				header:{
-					code: 4,
-					message: 'ERROR: user already in chat'
-				},
-				body:{}
-			});
-		}
-	}).catch((err) => {
-		res.json({
-			header:{
-				code: 1,
-				message: 'ERROR: sql'
-			},
-			body: {}
-		});
-	});
-});
-
-
-router.options('/addUser', function(req, res){
-	var origin = req.get('Origin');
-	// Check if origin is set
-	if(!origin){
-		console.log('no origin');
-		res.sendStatus(404);
-		return;
-	}
-	// Check if method that is requested is in the methods hash set
-	var method = req.get('Access-Control-Request-Method');
-	if(_OPTIONS["/addUser"]["HASHES"].has(method)){
-
-		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-		res.header("Access-Control-Max-Age", 86400);
-		res.sendStatus(200);
-		return;
-	}
-
-	// Send 404 if both of the above conditions are not met
-	res.sendStatus(404);
-});
-
-
-//get all chat ids by user id
-router.get('/byUser/:id', function(req, res){
-	chatsService.chatsGetChatsForUser(req.params.id).then((chatIDs) =>{
-		console.log("chat:" + JSON.stringify(chatIDs));
-		res.json({
-			header:{
-				code: 0,
-				message: 'success'
-			},
-			body:{
-				chatIDs : chatIDs
-			}
-		});
-	}).catch((err) =>{
-		res.json({
-			header:{
-				code: 1,
-				message: 'ERROR: sql'
-			},
-			body:{
-				err : err
-			}
-		});
-	});
-});
-
-
-router.options('/byUser', function(req, res){
-	var origin = req.get('Origin');
-	// Check if origin is set
-	if(!origin){
-		console.log('no origin');
-		res.sendStatus(404);
-		return;
-	}
-	// Check if method that is requested is in the methods hash set
-	var method = req.get('Access-Control-Request-Method');
-	if(_OPTIONS["/byUser"]["HASHES"].has(method)){
-
-		res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-		res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
-		res.header("Access-Control-Max-Age", 86400);
-		res.sendStatus(200);
-		return;
-	}
-
-	// Send 404 if both of the above conditions are not met
-	res.sendStatus(404);
-});
-
 
 //get messages for one chat
 /*
@@ -438,7 +328,7 @@ output:
 	messages: []
 }
 */
-router.get('/messages/:id', function(req, res){
+router.get('/:chatID/messages/:id', function(req, res){
 	console.log('Getting: ' + req.params.id);
 	chatsService.chatsGetMessagesForChat(req.params.id).then((msg) =>{
 		if(msg.length == 0){
@@ -486,7 +376,7 @@ input:
 	messageText: ''
 }
 */
-router.post('/messages', function(req, res){
+router.post('/:chatID/messages', function(req, res){
 	console.log(JSON.stringify(req.body));
 	chatsService.chatsAddMessageToChat(req.body.chatID,req.body.userID,req.body.messageText).then((msg) => {
 		//emit event to all users in chat
@@ -545,7 +435,7 @@ router.post('/messages', function(req, res){
 });
 
 
-router.options('/messages', function(req, res){
+router.options('/:chatID/messages', function(req, res){
 	var origin = req.get('Origin');
 	// Check if origin is set
 	if(!origin){
