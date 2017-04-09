29fe79ac868a36884fece40d39bfd482417bf11c
Cynthia <c_phan4@u.pacific.edu>
2017-04-09 01:58:08 -0700

    adding divs to info sidebar

--- apollo-frontend/src/components/Sidebar/info.js
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -4,7 +4,16 @@ export default class SidebarInfo extends Component {
     render() {
         return (
             <div className="sidebar-info-main">
-                Sidebar Info
+
+                <div className="sidebar-info-people">
+                </div>
+
+                <div className="sidebar-info-files">
+                </div>
+
+                <div className="sidebar-info-recent">
+                </div>
+
             </div>
         );
     }
