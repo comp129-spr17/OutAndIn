2503079258c45c3f71a5568102023b319f86d5a5
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-09 07:04:26 -0700

    Create new service functions

--- apollo-backend/services/sessions.js
+++ apollo-backend/services/sessions.js
@@ -10,3 +10,13 @@ exports.getUserIDBySessionToken = function(sessionToken){
 	return db.pool.query(sql, [sessionToken]);
 };
 
+exports.getSessionToken = function(sessionToken){
+	var sql = "SELECT user_id FROM user_sessions WHERE token = ?";
+	return db.pool.query(sql, [sessionToken]);
+};
+
+exports.deleteSessionToken = function(userID, sessionToken){
+	var sql = "DELETE FROM user_sessions WHERE user_id = ? AND token = ?";
+	return db.pool.query(sql, [userID, sessionToken]);
+};
+
