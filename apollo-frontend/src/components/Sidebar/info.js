063c5efe0310f2b7ffe8d31cab7a3a1d971505c4
Cynthia <c_phan4@u.pacific.edu>
2017-04-09 02:28:07 -0700

    done for tonight. don't know why the bottom borders of the inner divs won't extend towards the left to match with that vertical border. help???

--- apollo-frontend/src/components/Sidebar/info.js
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -6,15 +6,15 @@ export default class SidebarInfo extends Component {
             <div className="sidebar-info-main">
 
                 <div className="sidebar-info-people">
-                PEOPLE
+                People
                 </div>
 
                 <div className="sidebar-info-files">
-                FILES
+                Files
                 </div>
 
                 <div className="sidebar-info-recent">
-                RECENT FILES & IMAGES
+                Recent Images & Video
                 </div>
 
             </div>
