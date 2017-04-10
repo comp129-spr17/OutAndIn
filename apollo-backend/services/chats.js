b6207a2b534a18db94d0b5ad698bf24e2c615a15
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-09 19:40:45 -0700

    Created and updates chat services functions

--- apollo-backend/services/chats.js
+++ apollo-backend/services/chats.js
@@ -7,33 +7,23 @@
  */
 
 var db = require('../modules/database');
-var uuid = require('uuid/v1');
-
-/**
- * GenerateChatID
- * @description: Get the user by the username
- * @param: {string} username - Username of the user
- * @return: {promise} promise - SQL promise
- */
-exports.generateChatID = function(){
-	return uuid();
-};
+var uuid = require('uuid/v4');
 
 //create entry
-exports.createChat = function(chatID, userID){
-    var sql = "INSERT INTO chats VALUES('', ?, ?, '', '', '', '')";
+exports.createChat = function(userID, chatID){
+    var sql = "INSERT INTO chats VALUES('', ?, ?, 0, '', '', UNIX_TIMESTAMP(), UNIX_TIMESTAMP())";
     return db.pool.query(sql, [chatID, userID]);
 };
 
 //create entry
-exports.addUserToChat = function(chatID, userID){
+exports.addUserToChat = function(userID, chatID){
 	var sql = "INSERT INTO chat_users VALUES('', ?, ?)";
 	return db.pool.query(sql, [chatID, userID]);
 };
 
 //get entry by id
 exports.getChatByUUID = function(uuid){
-	var sql = "SELECT * FROM chats WHERE uuid = ?";
+	var sql = "SELECT uuid, creator_id, group_chat, name, avatar FROM chats WHERE uuid = ?";
 	return db.pool.query(sql, [uuid]);
 };
 
@@ -54,9 +44,22 @@ exports.chatsGetUsersForChat = function(chatID){
 	return db.pool.query(sql, [ChatID]);
 };
 
+// set chat status
+exports.setChatStatus = function(chatID){
+	var state = 0;
+	var sql = "INSERT INTO chat_status VALUES('', ?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())";
+	return db.pool.query(sql, [chatID, state]);
+};
+
 //get chat ids by user
 exports.getChatsForUser = function(userID){
 	var sql = "SELECT chats.uuid, chats.name, chats.avatar, chats.creator_id, chat_status.state, chats.created_at, chats.updated_at FROM chats, chat_users, chat_status WHERE chat_users.user_id = ? and chat_users.chat_id = chats.uuid and chat_status.chat_id = chat_users.chat_id"
+	return db.pool.query(sql, [userID]);
+};
+
+//get chat ids by user
+exports.getLimitedChatsForUser = function(userID){
+	var sql = "SELECT chats.uuid, chats.creator_id FROM chats, chat_users WHERE chat_users.user_id = ? and chat_users.chat_id = chats.uuid"
 	return db.pool.query(sql, [userID]);
 };
 
