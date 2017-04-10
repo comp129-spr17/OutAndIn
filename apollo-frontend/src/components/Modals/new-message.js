b002601cff6ce5e00545568522859623b438be6d
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-10 01:31:53 -0700

    Added an id to the new message modal

--- apollo-frontend/src/components/Modals/new-message.js
+++ apollo-frontend/src/components/Modals/new-message.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class NewMessageModal extends React.Component {
 	render(){
 		return (
-			<div>
-				<div id="new-message-modal-overlay"></div>
-				<div id="new-message-modal-container"></div>
+			<div id="new-message-modal">
+				<div className="new-message-modal-overlay"></div>
+				<div className="new-message-modal-container"></div>
 			</div>
 		)
 	}
