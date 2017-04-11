b395dc81b7ac8c5ff55778f0eaf1b6cd187a5546
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:29:48 -0700

    Pushing insignificant change. Storing changes

--- apollo-frontend/src/components/Modals/new-message.js
+++ apollo-frontend/src/components/Modals/new-message.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { client } from '../../modules/api-client';
 
 export default class NewMessageModal extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			users: []
+		};
+	}
 	render(){
 		return (
 			<div id="new-message-modal">
 				<div className="new-message-modal-overlay"></div>
-				<div id="new-message-modal-container"></div>
+				<div id="new-message-modal-container">
+					<input type="text" placeholder="Type friends name"/>
+				</div>
 			</div>
 		)
 	}
