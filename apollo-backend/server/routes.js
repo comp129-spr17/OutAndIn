e715af2b2c9211d539fccdf153f62080ff0534fb
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:39:08 -0700

    Including search api endpoint

--- apollo-backend/server/routes.js
+++ apollo-backend/server/routes.js
@@ -5,17 +5,20 @@ var users = require('../controllers/users');
 var chats = require('../controllers/chats');
 var register = require('../controllers/register');
 var session = require('../controllers/session');
+var search = require('../controllers/search');
 
 // Initialize any prep work before controller is used
 users.init();
 chats.init();
-session.init();
 register.init();
+session.init();
+search.init();
 
 // Register routes
 router.use('/chats', chats.router);
 router.use('/users', users.router);
 router.use('/register', register.router);
 router.use('/session', session.router);
+router.use('/search', search.router);
 
 module.exports = router;
