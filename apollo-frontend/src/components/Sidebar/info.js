392d756f744e21796310a411191279d75a4a94c5
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:34:05 -0700

    Removed font awesome js dep and used the already imported font awesome css lib

--- apollo-frontend/src/components/Sidebar/info.js
+++ apollo-frontend/src/components/Sidebar/info.js
@@ -1,64 +1,28 @@
 import React, { Component } from 'react';
-var FontAwesome = require('react-fontawesome');
 
 export default class SidebarInfo extends Component {
     render() {
         return (
             <div className="sidebar-info-main">
-
-                <div className="sidebar-info-people">
-                People
-
-                <FontAwesome
-                className="sidebar-info-addbutton"
-                name='plus-circle'
-                size='2x'/>
-
-                <div className="sidebar-info-inner">
-
+				<div className="sidebar-info-people">
+                	People
+					<i className="sidebar-info-addbutton fa fa-plus-circle fa-2x"></i>
+                	<div className="sidebar-info-inner"></div>
                 </div>
-                </div>
-
                 <div className="sidebar-info-files">
-                Files
-
-                <FontAwesome
-                className="sidebar-info-addbutton"
-                name='plus-circle'
-                size='2x'/>
-
-                <div className="sidebar-info-inner">
-
-                <FontAwesome
-                className="sidebar-info-fileiconaudio"
-                name= 'file-audio-o'
-                size='2x'/>
-
-                <FontAwesome
-                className="sidebar-info-fileiconvideo"
-                name='file-video-o'
-                size='2x'/>
-
-                <FontAwesome
-                className="sidebar-info-fileicontext"
-                name='file-text-o'
-                size='2x'/>
+                	Files
+					<i className="sidebar-info-addbutton fa fa-plus-circle fa-2x"></i>
+                	<div className="sidebar-info-inner">
+						<i className="sidebar-info-fileiconaudio fa fa-file-audio-o fa-2x"></i>
+						<i className="sidebar-info-fileiconvideo fa fa-file-video-o fa-2x"></i>
+						<i className="sidebar-info-fileicontext fa fa-file-text-o fa-2x"></i>
+                	</div>
                 </div>
-                </div>
-
                 <div className="sidebar-info-recent">
-                Recent Images & Video
-
-                <FontAwesome
-                className="sidebar-info-addbutton"
-                name='plus-circle'
-                size='2x'/>
-
-                <div className="sidebar-info-inner">
-                
+                	Recent Images & Video
+					<i className="sidebar-info-addbutton fa fa-plus-circle fa-2x"></i>
+                	<div className="sidebar-info-inner"></div>
                 </div>
-                </div>
-
             </div>
         );
     }
