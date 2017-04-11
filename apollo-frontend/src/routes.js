6026693ed4e267a2f35c3e2fed68cde074e6e21e
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:26:45 -0700

    Restructured the router

--- apollo-frontend/src/routes.js
+++ apollo-frontend/src/routes.js
@@ -1,38 +1,24 @@
 import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
-import Chat from './components/Chat';
-import Login from './components/Login';
-import LoginTest from './components/LoginTest';
-import Landing from './components/Landing';
-import Home from './components/Home';
-import SockTest from './components/SockTest';
-import NotFound from './components/NotFound';
-import Blank from './components/Layouts/blank';
-import Main from './components/Layouts/main';
-import Profile from './components/Profile';
-import ReduxTest from './containers/user';
-import Registration from './components/Registration';
-import RegistrationTest from './components/RegistrationTest';
-import ModalStatic from './components/ModalStatic';
+// Pages
+import App from './pages/App';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
+// Higher order component to protect privileged pages
+import AuthRequired from './utils/AuthRequired'; 
+
+// Router and the routes that utilize their respective components
 export default (
     <Router history={browserHistory} >
-        <Route component={Main} >
-            <Route path="/" component={Home} />
-            <Route path="/home" component={Home} />
-            <Route path="/chat" component={Chat} />
-            <Route path="/sockTest" component={SockTest} />
-            <Route path="/profile" component={Profile} />
-        </Route>
-        <Route component={Blank} >
-            <Route path="/redux" component={ReduxTest} />
+        <Route component={App} >
+        	<Route component={AuthRequired(Layout)} >
+            	<Route path="/" component={Home} />
+			</Route>
 			<Route path="/login" component={Login} />
-			<Route path="/loginTest" component={LoginTest} />
-            <Route path="/landing" component={Landing} />
-            <Route path="/register" component={Registration} />
-            <Route path="/registerTest" component={RegistrationTest} />
-            <Route path="/modalstatic" component={ModalStatic} />
         </Route>
         <Route path="*" component={NotFound} />
     </Router>
