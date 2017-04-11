cf4595195623a166a68be15cc77dd59e090b1eeb
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:26:25 -0700

    Created frontend authentication middleware

---
+++ apollo-frontend/src/utils/AuthRequired.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Landing from '../components/Landing';
+
+export default function(ComposedComponent){
+	class Authenticate extends Component {
+		componentWillMount(){
+			console.log(this.context.router.location.pathname);
+			if(!this.props.isAuthenticated && !localStorage.getItem("token") && !this.context.router.location.pathname == "/"){
+				this.context.router.push("/login");
+			}	
+		}
+		
+		componentWillUpdate(nextProps){
+			if(!nextProps.isAuthenticated){
+				this.context.router.push("/");
+			}	
+		}
+
+		render(){
+			if(this.context.router.location.pathname == "/" && !this.props.isAuthenticated && !localStorage.getItem("token")){
+				return (
+					<Landing { ...this.props } />
+				);	
+			} else {
+				return (
+					<ComposedComponent { ...this.props } />
+				);
+			}
+		}
+	}
+
+	Authenticate.propTypes = {
+		isAuthenticated: PropTypes.bool.isRequired	
+	};
+
+	Authenticate.contextTypes = {
+		router: PropTypes.object.isRequired
+	};
+
+	function mapStateToProps(state){
+		return {
+			isAuthenticated: state.login.isAuthenticated
+		};
+	}
+	
+	return connect(mapStateToProps, null)(Authenticate);
+}
