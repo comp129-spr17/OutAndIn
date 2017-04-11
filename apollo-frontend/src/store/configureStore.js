32f69f41f809d852507e4f18d9a75eccaaef81d6
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-11 06:30:33 -0700

    Removed unnecessary promise middleware for redux

--- apollo-frontend/src/store/configureStore.js
+++ apollo-frontend/src/store/configureStore.js
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { logger } from 'redux-logger';
 import reducers from '../reducers';
-import promise from 'redux-promise-middleware';
 
 export default function configureStore(){
     return createStore(
         reducers,
         {},
-        applyMiddleware(logger, thunk, promise())
+        applyMiddleware(logger, thunk)
     );
 }
