5e73ea92e614320066e11da2e0308cda7cd767b9
MalvikaSriram <m_sriram@u.pacific.edu>
2017-04-12 23:39:35 -0700

    Im really bad at css and design, so this is gonna take a while. baby steps. going to continue later when im less tired.

--- apollo-frontend/src/components/Profile/index.js
+++ apollo-frontend/src/components/Profile/index.js
@@ -1,5 +1,16 @@
 import React, { PropTypes, Component } from 'react';
 
+class Header extends Component{
+    render() {
+        return (
+                <div className='profile-header'>
+                    <h3>My Profile</h3>
+                </div>
+            );
+    }
+}
+
+
 export default class Friends extends Component {
     constructor () { 
         super();
@@ -17,30 +28,33 @@ export default class Friends extends Component {
     }
  render() {
         return (
-            <div className='profile-container'> 
-                <div >
-                    <div className='profile-avatar'>
-                        <img  src={ this.state.user.avatar } alt="User Image"/>
-                    </div>
-                    <div  >
-                        <p className='profile-name' >{this.state.user.name} </p>
-                        <p className='profile-handle' > {this.state.user.handle} </p>
-                    </div>
-                </div>
-                <div className='profile-otherinfo'>
-                    <p> Phone : {this.state.user.phoneNumber}  </p>
-                    <p> Birthday : {this.state.user.dob}  </p>
-                    <p> Where I Live : {this.state.user.address}  </p>
-                    <div>
-                        <p>Friends </p>
-                        <div className='profile-friendlist'> 
-                             { this.state.user.friends.map((friend, k) => {
-                                return <img  src={ friend }  key={k} alt="User Image"/>
-                             })}
+            <div>
+         <Header></Header>
+                <div className='profile-container'> 
+                    <div >
+                        <div className='profile-avatar'>
+                            <img  src={ this.state.user.avatar } alt="User Image"/>
+                        </div>
+                        <div  >
+                            <p className='profile-name' >{this.state.user.name} </p>
+                            <p className='profile-handle' > {this.state.user.handle} </p>
                         </div>
                     </div>
-                </div>
+                    <div className='profile-otherinfo'>
+                        <p> Phone : {this.state.user.phoneNumber}  </p>
+                        <p> Birthday : {this.state.user.dob}  </p>
+                        <p> Where I Live : {this.state.user.address}  </p>
+                        <div>
+                            <p>Friends </p>
+                            <div className='profile-friendlist'> 
+                                 { this.state.user.friends.map((friend, k) => {
+                                    return <img  src={ friend }  key={k} alt="User Image"/>
+                                 })}
+                            </div>
+                        </div>
+                    </div>
 
+                </div>
             </div>
         );
     }
