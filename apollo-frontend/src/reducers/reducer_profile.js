a2056e483fc49da313649a0e3e60d676eec6270b
MalvikaSriram <m_sriram@u.pacific.edu>
2017-04-16 11:20:16 -0700

    fixed reducer issue. initial state was modified. Reverted it to the original structure in the profile reducer file. Profile design works now

--- apollo-frontend/src/reducers/reducer_profile.js
+++ apollo-frontend/src/reducers/reducer_profile.js
@@ -7,13 +7,23 @@ import {
 } from '../actions/profile';
 
 
+// const INITIAL_STATE = {
+// 	user: {
+// 		name: '',
+// 		avatar: ''
+// 	},
+// 	loading: false,
+// 	error: ''
+// };
+
 const INITIAL_STATE = {
-	user: {
-		name: '',
-		avatar: ''
-	},
-	loading: false,
-	error: ''
+    name:'Abc',
+    handle:'@abc',
+    avatar:'/img/avatar1.jpg',
+    phoneNumber:'+1-2345678',
+    dob:'12-01-2011',
+    address:'Here and There',
+    friends:['/img/avatar1.jpg','/img/avatar2.jpg','/img/avatar3.jpg']
 };
 
 export default function(state = INITIAL_STATE, action){
@@ -58,4 +68,3 @@ export default function(state = INITIAL_STATE, action){
 	}	
 	return state;
 };
-
