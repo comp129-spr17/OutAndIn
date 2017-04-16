690e845c7c9afe0c781d403fde0c11f1f7c57a4f
MalvikaSriram <m_sriram@u.pacific.edu>
2017-04-16 10:15:28 -0700

    Reverting to previous code. blank profile screen, no clue why

--- apollo-frontend/src/components/Profile/index.js
+++ apollo-frontend/src/components/Profile/index.js
@@ -13,32 +13,32 @@ class Header extends Component{
 
 
 export default class Profile extends Component {
-	constructor(){
-		super();
-	}
-	render() {
+    constructor(){
+        super();
+    }
+    render() {
         return (
-        	<div className="content">
-         		<Header></Header>
+            <div className="content">
+                <Header></Header>
                 <div className='profile-container'> 
-                   	<div >
-                       	<div className='profile-avatar'>
-                           	<img  src={ this.props.profile.avatar } alt="User Image"/>
-                       	</div>
-                       	<div  >
-                           	<p className='profile-name' >{this.props.profile.name} </p>
-                           	<p className='profile-handle' > {this.props.profile.handle} </p>
-                       	</div>
-                   	</div>
-                   	<div className='profile-otherinfo'>
-                       	<p> Phone : {this.props.profile.phoneNumber}  </p>
-                       	<p> Birthday : {this.props.profile.dob}  </p>
-                       	<p> Where I Live : {this.props.profile.address}  </p>
-                       	<div>
-                           	<p>Friends </p>
-                           	<div className='profile-friendlist'> 
-                               	{ this.props.profile.friends.map((friend, k) => {
-                                 	 return <img  src={ friend }  key={k} alt="User Image"/>
+                    <div >
+                        <div className='profile-avatar'>
+                            <img  src={ this.props.profile.avatar } alt="User Image"/>
+                        </div>
+                        <div  >
+                            <p className='profile-name' >{this.props.profile.name} </p>
+                            <p className='profile-handle' > {this.props.profile.handle} </p>
+                        </div>
+                    </div>
+                    <div className='profile-otherinfo'>
+                        <p> Phone : {this.props.profile.phoneNumber}  </p>
+                        <p> Birthday : {this.props.profile.dob}  </p>
+                        <p> Where I Live : {this.props.profile.address}  </p>
+                        <div>
+                            <p>Friends </p>
+                            <div className='profile-friendlist'> 
+                                { this.props.profile.friends.map((friend, k) => {
+                                     return <img  src={ friend }  key={k} alt="User Image"/>
                                 })}
                             </div>
                         </div>
