e5ff774c34827061054a076aaa879c45595434e5
pranavt61 <p_thirunavukkarasu@u.pacific.edu>
2017-04-19 00:45:01 -0700

    chats working with redux

---
+++ apollo-frontend/src/pages/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SidebarContainer from '../containers/SidebarContainer';
+
+class Sidebar extends Component{
+	render(){
+		return (
+			<SidebarContainer />				
+		);
+	};
+};
+
+export default Sidebar;
