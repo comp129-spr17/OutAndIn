2170e2dbd61d74674bd926e945b514c629edf67a
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-04-20 20:54:48 -0700

    Logout works now

--- apollo-frontend/src/components/Header/index.js
+++ apollo-frontend/src/components/Header/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'; 
+import { client } from '../../modules/api-client';
 
 var jwt_decode = require('jwt-decode');
 
 export default class Header extends Component { 
     constructor(){
 		super();
-
+		this.logout = this.logout.bind(this);
 		this.handleUserProfile = this.handleUserProfile.bind(this);
 	}
+
+	logout(e){
+		e.preventDefault();
+		var self = this;
+		client.sessionLogout().then((res) => {
+			localStorage.removeItem("token");
+			console.log("logged out");
+			window.location.href = "/";
+		}).catch((err) => {
+			alert("Logout Unsuccessful. Try again.");	
+		});
+	} 
 
 	handleUserProfile(){
 		let id = jwt_decode(localStorage.getItem('token')).uid;
@@ -22,7 +35,7 @@ export default class Header extends Component {
                 </div>
                 <div className="info-container">
                     <div className="info-links">
-                        <span><a href=""><i className="fa fa-sign-out"></i></a></span>
+                        <span><i className="fa fa-sign-out" onClick={this.logout}></i></span>
                     </div>
                     <div className="info-user" onClick={this.handleUserProfile}>
                         <div className="info-img">
